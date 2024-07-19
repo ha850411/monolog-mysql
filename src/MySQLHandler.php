9b9a9630770b7d9b7b21146d1b99c38e837b915e
eason.lee <eason.lee@104.com.tw>
2024-07-19 14:25:49 +0800

    init

---
+++ src/MySQLHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MySQLHandler;
+
+use Monolog\Handler\AbstractProcessingHandler;
+
+class MySQLHandler extends AbstractProcessingHandler
+{
+    protected function write($connection, array $record): void
+    {
+        $connection->insert([
+            'channel' => $record['channel'],
+            'level' => $record['level'],
+            'message' => $record['message'],
+            'time' => $record['datetime']->format('Y-m-d H:i:s')
+        ]);
+    }
+}
